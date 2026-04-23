44b005d419821d081e55ebb1d70dbba7c69a4b54
deekshith-sclera <deekshith.devadas@sclera.com>
2026-04-23 16:49:06 +0530

    feat:SEO optimisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from  '@/app/ui/fonts';
+import { Metadata} from "next";
 
+
+export const metadata: Metadata={
+    title: {
+        template: '%s | Invoice Dashboard',
+        default:'Dashboard'
+    },
+    description:'Invoice Dashboard Learning',
+    // metadataBase
+}
 
 export default function RootLayout({
                                      children,
